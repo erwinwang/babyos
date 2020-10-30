612256a7f9f5d3b0396f9708a3dfbb2800d27951
erwinwang <erwin.wang@qq.com>
2020-10-30 19:03:48 +0800

    add features gdt idt and timer

--- init/entry.c
+++ init/entry.c
@@ -1,15 +1,22 @@
 #include "console.h"
 #include "debug.h"
+#include "gdt.h"
+#include "idt.h"
+#include "timer.h"
 
 int kern_entry()
 {
     init_debug();
+    init_gdt();
+    init_idt();
 
     console_clear();
-
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 
-    panic("test");
+    init_timer(200);
+
+    // 开启中断
+    asm volatile ("sti");
 
     return 0;
 }
