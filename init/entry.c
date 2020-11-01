08dd2a78f89bdc31711b83f0ca4c5cab606c34f0
erwinwang <erwin.wang@qq.com>
2020-11-01 21:30:38 +0800

    add mm

--- init/entry.c
+++ init/entry.c
@@ -4,12 +4,14 @@
 #include "idt.h"
 #include "timer.h"
 #include "kbd_ps2.h"
+#include "pmm.h"
 
 int kern_entry()
 {
     init_debug();
     init_gdt();
     init_idt();
+    // init_pmm();
 
     console_clear();
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
@@ -19,6 +21,12 @@ int kern_entry()
 
     // 开启中断
     asm volatile ("sti");
+
+    printk("kernel in memory start: 0x%08X\n", kern_start);
+    printk("kernel in memory end:   0x%08X\n", kern_end);
+    printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start + 1023) / 1024);
+    
+    show_memory_map();
 
     return 0;
 }
